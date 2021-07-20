ef4d5df9f4153efa816f1b5d0c277c58885fcb72
simon-fu <simonok@139.com>
2021-07-20 17:14:27 +0800

    bench-tokio support bound mpsc

--- benches/bench-tokio.rs
+++ benches/bench-tokio.rs
@@ -1,12 +1,10 @@
-// cargo bench --bench bench-tokio
+// cargo bench --bench bench-tokio -- mpsc-unbounded
 
-// use std::time::Instant;
+use std::vec;
 
 use criterion::*;
 
-async fn bench_mpsc_unbounded_channel(nsends: u64, nmsgs: u64, ngroups: u64) {
-    // let now = Instant::now();
-
+async fn run_mpsc_unbounded_channel(nsends: u64, nmsgs: u64, ngroups: u64) {
     let (tx_final, mut rx_final) = tokio::sync::mpsc::unbounded_channel();
     for _ in 0..ngroups {
         let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
@@ -15,7 +13,10 @@ async fn bench_mpsc_unbounded_channel(nsends: u64, nmsgs: u64, ngroups: u64) {
             let tx0 = tx.clone();
             let _h1 = tokio::spawn(async move {
                 for i in 0..nmsgs {
-                    let _ = tx0.send(i);
+                    let r = tx0.send(i);
+                    if let Err(e) = r {
+                        println!("send error {:?}", e);
+                    }
                 }
             });
         }
@@ -40,94 +41,110 @@ async fn bench_mpsc_unbounded_channel(nsends: u64, nmsgs: u64, ngroups: u64) {
             break;
         }
     }
+}
 
-    // let _ = tokio::join!(h2);
+fn bench_mpsc_unbounded_channel(
+    group: &mut BenchmarkGroup<'_, measurement::WallTime>,
+    nsends: u64,
+    nmsgs: u64,
+    ngroups: u64,
+) {
+    group.throughput(Throughput::Elements(nsends * nmsgs * ngroups));
+    group.bench_function(
+        format!("mpsc-unbounded-{}x{}x{}", nsends, nmsgs, ngroups),
+        |b| {
+            b.to_async(tokio::runtime::Runtime::new().unwrap())
+                .iter(|| async {
+                    run_mpsc_unbounded_channel(nsends, nmsgs, ngroups).await;
+                    // rust_threeq::tq3::measure_async("mpsc_unbounded_aaa", nsends*nmsgs*ngroups, run_mpsc_unbounded_channel(nsends, nmsgs, ngroups)).await;
+                })
+        },
+    );
+}
 
-    // let elapsed = now.elapsed();
-    // drop(now);
-    // let num = (nsends * ngroups * nmsgs) as u128;
-    // let each = elapsed.as_nanos() / num;
-    // let rate = num * 1_000_000_000 / elapsed.as_nanos();
-    // println!(
-    //     "{}: num {}, elapsed {:?}, each {} ns, estimate {}/sec",
-    //     "mpsc unbound", num, elapsed, each, rate
-    // );
+async fn run_mpsc_bounded_channel(nsends: u64, nmsgs: u64, ngroups: u64, qsize: usize) {
+    let (tx_final, mut rx_final) = tokio::sync::mpsc::channel(ngroups as usize);
+    for _ in 0..ngroups {
+        let (tx, mut rx) = tokio::sync::mpsc::channel(qsize);
+
+        for _ in 0..nsends {
+            let tx0 = tx.clone();
+            let _h1 = tokio::spawn(async move {
+                for i in 0..nmsgs {
+                    let r = tx0.send(i).await;
+                    if let Err(e) = r {
+                        println!("send error {:?}", e);
+                    }
+                }
+            });
+        }
+        drop(tx);
+
+        {
+            let tx0 = tx_final.clone();
+            let _h2 = tokio::spawn(async move {
+                loop {
+                    if let None = rx.recv().await {
+                        break;
+                    }
+                }
+                let _ = tx0.send(());
+            });
+        }
+    }
+    drop(tx_final);
+
+    loop {
+        if let None = rx_final.recv().await {
+            break;
+        }
+    }
+}
+
+fn bench_mpsc_bounded_channel(
+    group: &mut BenchmarkGroup<'_, measurement::WallTime>,
+    nsends: u64,
+    nmsgs: u64,
+    ngroups: u64,
+    qsize: usize,
+) {
+    group.throughput(Throughput::Elements(nsends * nmsgs * ngroups));
+    group.bench_function(
+        format!("mpsc-bounded-{}x{}x{}x{}", nsends, nmsgs, ngroups, qsize),
+        |b| {
+            b.to_async(tokio::runtime::Runtime::new().unwrap())
+                .iter(|| async {
+                    run_mpsc_bounded_channel(nsends, nmsgs, ngroups, qsize).await;
+                    // rust_threeq::tq3::measure_async("mpsc_bounded_aaa", nsends*nmsgs*ngroups, run_mpsc_bounded_channel(nsends, nmsgs, ngroups, qsize)).await;
+                })
+        },
+    );
+}
+
+fn bench_mpsc(group: &mut BenchmarkGroup<'_, measurement::WallTime>) {
+    let qsize = 2048usize;
+    let args = vec![
+        (1, 1_000_000, 1, qsize),
+        (1, 100_000, 10, qsize),
+        (1, 10_000, 100, qsize),
+        (1, 1_000, 1000, qsize),
+        (100, 10_000, 1, qsize),
+        (100, 10_000, 10, qsize),
+    ];
+
+    for v in &args {
+        bench_mpsc_unbounded_channel(group, v.0, v.1, v.2);
+    }
+
+    for v in &args {
+        bench_mpsc_bounded_channel(group, v.0, v.1, v.2, v.3);
+    }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("bench-tokio");
 
-    let nsends = 1u64;
-    let nmsg = 1_000_000u64;
-    let ngroups = 1u64;
-    group.throughput(Throughput::Elements(nsends * nmsg * ngroups));
-    group.bench_function(
-        format!("mpsc-unbounded-{}x{}x{}", nsends, nmsg, ngroups),
-        |b| {
-            b.to_async(tokio::runtime::Runtime::new().unwrap())
-                .iter(|| async { bench_mpsc_unbounded_channel(nsends, nmsg, ngroups).await })
-        },
-    );
-
-    let nsends = 1u64;
-    let nmsg = 100_000u64;
-    let ngroups = 10u64;
-    group.throughput(Throughput::Elements(nsends * nmsg * ngroups));
-    group.bench_function(
-        format!("mpsc-unbounded-{}x{}x{}", nsends, nmsg, ngroups),
-        |b| {
-            b.to_async(tokio::runtime::Runtime::new().unwrap())
-                .iter(|| async { bench_mpsc_unbounded_channel(nsends, nmsg, ngroups).await })
-        },
-    );
-
-    let nsends = 1u64;
-    let nmsg = 10_000u64;
-    let ngroups = 100u64;
-    group.throughput(Throughput::Elements(nsends * nmsg * ngroups));
-    group.bench_function(
-        format!("mpsc-unbounded-{}x{}x{}", nsends, nmsg, ngroups),
-        |b| {
-            b.to_async(tokio::runtime::Runtime::new().unwrap())
-                .iter(|| async { bench_mpsc_unbounded_channel(nsends, nmsg, ngroups).await })
-        },
-    );
-
-    let nsends = 1u64;
-    let nmsg = 1_000u64;
-    let ngroups = 1000u64;
-    group.throughput(Throughput::Elements(nsends * nmsg * ngroups));
-    group.bench_function(
-        format!("mpsc-unbounded-{}x{}x{}", nsends, nmsg, ngroups),
-        |b| {
-            b.to_async(tokio::runtime::Runtime::new().unwrap())
-                .iter(|| async { bench_mpsc_unbounded_channel(nsends, nmsg, ngroups).await })
-        },
-    );
-
-    let nsends = 100u64;
-    let nmsg = 10_000u64;
-    let ngroups = 1u64;
-    group.throughput(Throughput::Elements(nsends * nmsg * ngroups));
-    group.bench_function(
-        format!("mpsc-unbounded-{}x{}x{}", nsends, nmsg, ngroups),
-        |b| {
-            b.to_async(tokio::runtime::Runtime::new().unwrap())
-                .iter(|| async { bench_mpsc_unbounded_channel(nsends, nmsg, ngroups).await })
-        },
-    );
-
-    let nsends = 100u64;
-    let nmsg = 1000u64;
-    let ngroups = 10u64;
-    group.throughput(Throughput::Elements(nsends * nmsg * ngroups));
-    group.bench_function(
-        format!("mpsc-unbounded-{}x{}x{}", nsends, nmsg, ngroups),
-        |b| {
-            b.to_async(tokio::runtime::Runtime::new().unwrap())
-                .iter(|| async { bench_mpsc_unbounded_channel(nsends, nmsg, ngroups).await })
-        },
-    );
+    bench_mpsc(&mut group);
 
     group.finish();
 }
