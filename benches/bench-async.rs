3c37a5083fb299872ba0b727a1e20ddcd5374681
simon-fu <simonok@139.com>
2021-07-23 13:27:19 +0800

    support more case for bench hub

--- benches/bench-async.rs
+++ benches/bench-async.rs
@@ -1,36 +1,67 @@
 // cargo bench --bench bench-async
 
+use clap::Clap;
 use histogram::Histogram;
 use rust_threeq::tq3;
 use rust_threeq::tq3::hub;
 use std::sync::Arc;
-use std::time::Instant;
+use std::time::{Duration, Instant};
+use tokio::sync::mpsc;
 use tracing::info;
 
-async fn run_hub_1_to_n(num: u64, hist: &mut Histogram) {
-    type Message = Instant;
-    pub type Subscriptions = hub::LatestFirstQue<Arc<Message>>;
-    pub type Hub = hub::Hub<Arc<Message>, u64, Subscriptions>;
+#[derive(Clap, Debug)]
+#[clap(name = "bench-async", author, about, version)]
+struct CmdArgs {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
 
-    let hub = Hub::new();
-    let (tx_final, mut rx_final) = tokio::sync::mpsc::unbounded_channel();
+#[derive(Clap, Debug)]
+enum SubCommand {
+    Hub(HubArgs),
+}
 
+#[derive(Clap, Debug)]
+struct HubArgs {
+    #[clap(long, long_about = "num of subscribers", default_value = "100000")]
+    subs: u64,
+
+    #[clap(long, long_about = "num of hubs", default_value = "0")]
+    hubs: u64,
+
+    #[clap(long)]
+    bench: bool,
+}
+
+type Message = Instant;
+pub type Subscriptions = hub::LatestFirstQue<Arc<Message>>;
+pub type Hub = hub::Hub<Arc<Message>, u64, Subscriptions>;
+
+async fn spawn_subs(hub: &mut Hub, num: u64, tx: &mpsc::UnboundedSender<Duration>) {
     for n in 0..num {
         let subs = Subscriptions::new(1);
         let subs = Arc::new(subs);
         hub.add(n, subs.clone()).await;
-        let tx0 = tx_final.clone();
+        let tx0 = tx.clone();
         let _h1 = tokio::spawn(async move {
             let r = subs.recv().await.unwrap();
             let _r = tx0.send(Instant::now() - *r.0);
         });
     }
-    drop(tx_final);
+}
 
-    hub.push(&Arc::new(Instant::now())).await;
+async fn run_hubs(hubs: u64, subs: u64, hist: &mut Histogram) {
+    let (tx, mut rx) = mpsc::unbounded_channel();
+
+    for _ in 0..hubs {
+        let mut hub = Hub::new();
+        spawn_subs(&mut hub, subs, &tx).await;
+        hub.push(&Arc::new(Instant::now())).await;
+    }
+    drop(tx);
 
     loop {
-        match rx_final.recv().await {
+        match rx.recv().await {
             Some(d) => {
                 let _r = hist.increment(d.as_nanos() as u64);
             }
@@ -41,24 +72,52 @@ async fn run_hub_1_to_n(num: u64, hist: &mut Histogram) {
     }
 }
 
-async fn measure_hub_1_to_n(num: u64) {
-    let name = "Hub 1-to-N";
+async fn measure_hubs(name: &str, hubs: u64, subs: u64) {
+    info!("{}: hubs {}, subs {}", name, hubs, subs);
     let mut hist = Histogram::new();
-    tq3::measure_and_print(&name, num, run_hub_1_to_n(num, &mut hist)).await;
+    tq3::measure_and_print(name, hubs * subs, run_hubs(hubs, subs, &mut hist)).await;
     tq3::histogram::print_duration(&name, &hist);
+}
+
+async fn bench_hub(args: HubArgs) {
+    {
+        info!("-");
+        info!("warm up ...");
+        let mut hist = Histogram::new();
+        run_hubs(1, 10000, &mut hist).await;
+        info!("warm up done");
+    }
+
+    if args.hubs == 0 {
+        info!("-");
+        measure_hubs("Hub 1-to-N1", 1, 100000).await;
+
+        info!("-");
+        measure_hubs("Hub M-to-N2", 10, 100000).await;
+
+        info!("-");
+        measure_hubs("Hub M-to-N1", 10, 10000).await;
+    } else {
+        info!("-");
+        measure_hubs("Hub latency", 1, 100000).await;
+    }
 }
 
 #[tokio::main]
 async fn main() {
     rust_threeq::tq3::log::tracing_subscriber::init();
 
-    {
-        info!("warm up ...");
-        let mut hist = Histogram::new();
-        run_hub_1_to_n(10000, &mut hist).await;
-        info!("warm up done");
-    }
+    info!("-");
 
-    let num = 100_000;
-    measure_hub_1_to_n(num).await;
+    let args: Vec<String> = std::env::args().collect();
+    info!("{:?}", args);
+
+    let args = CmdArgs::parse();
+    info!("args={:?}", args);
+
+    match args.subcmd {
+        SubCommand::Hub(sub_args) => {
+            bench_hub(sub_args).await;
+        }
+    }
 }
