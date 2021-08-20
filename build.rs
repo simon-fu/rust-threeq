39c3b84dac32b4bbcebedf2c2d2128a8cb7e9e7e
simon-fu <simonok@139.com>
2021-08-20 17:33:28 +0800

    only run build.rs when proto/service.proto changed

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=proto/service.proto");
     tonic_build::compile_protos("proto/service.proto")?;
     Ok(())
 }
